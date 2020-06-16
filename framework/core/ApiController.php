297eb50466a53a301b0931f4acd55c9e09700e22
narmings <narmings@code.edu.az>
2020-06-16 22:06:21 +0400

    Fixed authorization typo

--- framework/core/ApiController.php
+++ framework/core/ApiController.php
@@ -5,10 +5,11 @@ require_once  dirname(__DIR__) . DS .'vendor/autoload.php';
 class ApiController
 {
     protected $authorize = true;
-    protected $loggedIn = false;
+    protected $loggedIn = null;
 
     function __construct()
     {
-        if ($this->authorize == true) $this->loggedIn = Auth::Authorize();
+        if ($this->authorize == true) 
+            $this->loggedIn = Auth::Authorize();
     }
 }
