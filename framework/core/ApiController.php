cb14f90b3aabbea8dbd3e6d25fe994ed636a7b5b
narmings <narmings@code.edu.az>
2020-06-16 22:02:42 +0400

    fixed logged in in controller base

--- framework/core/ApiController.php
+++ framework/core/ApiController.php
@@ -5,6 +5,7 @@ require_once  dirname(__DIR__) . DS .'vendor/autoload.php';
 class ApiController
 {
     protected $authorize = true;
+    protected $loggedIn = false;
 
     function __construct()
     {
